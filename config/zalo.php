976200be4425f11df063cddd1a3adb8ba405a644
KIMTUNG <nguyenkim08@gmail.com>
2020-05-23 21:06:55 +0700

    fix: callback url

--- config/zalo.php
+++ config/zalo.php
@@ -3,5 +3,5 @@
 return [
     'app_id' => '801230195877917123',
     'app_secret' => 'P4dPh2UYHKJa7msSIw76',
-    'callback_url' => 'https://soiket.com/callback'
+    'callback_url' => 'https://www.soiket.com/callback'
 ];
