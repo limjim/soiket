fe54931d7e3589b038abc13993a0ebc40649cb75
KIMTUNG <nguyenkim08@gmail.com>
2020-05-24 08:01:18 +0700

    get access token

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,13 +32,11 @@ class HomeController extends Controller
     public function callback(\Request $request) {
         $oauthCode = isset($_GET['code']) ? $_GET['code'] : "THIS NOT CALLBACK PAGE !!!";
         $accessToken = $this->helper->getAccessToken($this->config['callback_url']); // get access token
-        $params = ['offset' => 0, 'limit' => 10, 'fields' => "id, name, birthday, picture"];
-        $response = $this->zalo->get(ZaloEndPoint::API_GRAPH_FRIENDS, $accessToken, $params);
-        $result = $response->getDecodedBody(); 
         echo '<pre>';
-        print_r($result);
+        print_r($accessToken);
         echo '</pre>';
         exit();
+        
     }
 
 }
