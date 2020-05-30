d1958df39ba58daa5b51c03fa46ff8b49d855b94
KIMTUNG <nguyenkim08@gmail.com>
2020-05-30 22:17:18 +0700

    fix: password

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => 'soiket_www',
             'username' => 'root',
-            'password' => '',
+            'password' => 'qwe123mnb',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
